b8c28237b3fd655cbcfa64a93f45bea43721bc00
ly16419 <1641911975@qq.com>
2021-01-10 10:13:36 +0800

    值传递

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] agre){
-        System.out.print("成功");
+        //值的传递，不改变a的值
+        int a=3;
+        m1(a);
+        System.out.println("aaa" + a);
+    }
+    public static void m1(int a){
+        a=5;
+        System.out.println("ccc" + a);
     }
 }
