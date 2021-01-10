911bbbab6f1b8832cd3e285edbb246a85c42c7de
ly16419 <1641911975@qq.com>
2021-01-10 10:13:48 +0800

    引用传递

---
+++ src/ArrTeest.java
@@ -0,0 +1,16 @@
+public class ArrTeest {
+    public static void main(String[] agre){
+        //引用传递
+        int[] a=new int[]{1,2,3};
+        m1(a);
+        for (int i=0;i<a.length;i++){
+            System.out.println("a[" + i + "]" + a[i]);
+        }
+    }
+    public static void m1(int[] a){
+        a[2]=98;
+        for (int i=0;i<a.length;i++){
+            System.out.println("a[" + i + "]" + a[i]);
+        }
+    }
+}
